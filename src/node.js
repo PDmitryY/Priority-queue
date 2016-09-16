c9ccd407163280a75de959ec135d6b5fbf981781
Dmitry Petrochenkov <demon-st2002@yandex.ru>
2016-09-16 23:55:36 +0300

    change constructor

--- src/node.js
+++ src/node.js
@@ -2,10 +2,13 @@ class Node {
 	constructor(data, priority) {
 		this.data = data;
 		this.priority = priority;
-
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
+					
 
 	}
 
